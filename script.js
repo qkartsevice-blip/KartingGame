7c8da6511168c4a25f9325f2628df6e4c37c6648
qkartsevice-blip <qkartsevice@gmail.com>
2025-08-28 17:02:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -56,8 +56,8 @@ const allQuestions = [
         correctAnswer: "衛生問題"
     },
     {
-        question: "小小孩開小小卡丁車的好處?",
-        options: ["訓練專注力"<br>"與判斷力", "訓練動作協調"<br>"與核心控制", "以上皆是"],
+        question: "小小孩開小小卡丁車可以訓練?",
+        options: ["專注力與判斷力", "動作協調與核心控制", "以上皆是"],
         correctAnswer: "以上皆是"
     }
 ];
@@ -338,4 +338,5 @@ canvas.addEventListener('click', (event) => {
 // 啟動遊戲迴圈與資源載入
 gameLoop();
 loadAssets();
+
 
